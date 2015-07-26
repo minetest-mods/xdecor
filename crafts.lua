1c731042e67b66336574d639dc78292113fe9e8e
Christian Wischenbart <c.wischenbart@bitoxide.com>
2015-07-26 02:42:17 +0200

    added alternative xdecor:cauldron recipe with water bucket

--- crafts.lua
+++ crafts.lua
@@ -63,6 +63,20 @@ minetest.register_craft({
 	}
 })
 
+if minetest.get_modpath("bucket") then
+	minetest.register_craft({
+	output = "xdecor:cauldron",
+	recipe = {
+		{"default:iron_lump", "", "default:iron_lump"},
+		{"default:iron_lump", "bucket:bucket_water", "default:iron_lump"},
+		{"default:iron_lump", "default:iron_lump", "default:iron_lump"}
+	},
+	replacements = {
+		{"bucket:bucket_water", "bucket:bucket_empty"}
+	}
+})
+end
+
 minetest.register_craft({
 	output = "xdecor:chair",
 	recipe = {
