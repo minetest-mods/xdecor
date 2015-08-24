be05c7ef776687aa2b98cbf22a7eb662b1b892d3
Tim <t4im@users.noreply.github.com>
2015-08-24 18:51:48 +0200

    use inventory lists as interface for the worktable cut mechanism

--- worktable.lua
+++ worktable.lua
@@ -1,9 +1,7 @@
 local worktable = {}
 local xbg = default.gui_bg..default.gui_bg_img..default.gui_slots
-local concat = table.concat
 
 local material = {
-	"cloud", -- Only used for the formspec display.
 	"wood", "junglewood", "pine_wood", "acacia_wood",
 	"tree", "jungletree", "pine_tree", "acacia_tree",
 	"cobble", "mossycobble", "desert_cobble",
@@ -32,95 +30,52 @@ local def = { -- Node name, nodebox shape.
 
 function worktable.crafting(pos)
 	local meta = minetest.get_meta(pos)
-	local inv = meta:get_inventory()
-
-	local f = {"size[8,7;]"..xbg..
+	return "size[8,7;]"..xbg..
 		"list[current_player;main;0,3.3;8,4;]"..
 		"image[5,1;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
 		"list[current_player;craft;2,0;3,3;]"..
-		"list[current_player;craftpreview;6,1;1,1;]"}
-	local formspec = concat(f)
-	return formspec
+		"list[current_player;craftpreview;6,1;1,1;]"
 end
 
 function worktable.storage(pos)
 	local meta = minetest.get_meta(pos)
-	local inv = meta:get_inventory()
-
 	local f = "size[8,7]"..xbg..
 		"list[context;storage;0,0;8,2;]list[current_player;main;0,3.25;8,4;]"
-	inv:set_size("storage", 8*2)
+	meta:get_inventory():set_size("storage", 8*2)
 	return f
 end
 
 function worktable.construct(pos)
 	local meta = minetest.get_meta(pos)
 	local inv = meta:get_inventory()
-	local row1, row2, row3 = {}, {}, {}
 
-	for i = 1, 4 do
-		row1[#row1+1] = "item_image_button["..(i-1)..
-				",0;1,1;xdecor:"..def[i][1].."_cloud;"..def[i][1]..";]"
-	end
-	row1 = concat(row1)
-	for j = 5, 8 do
-		row2[#row2+1] = "item_image_button["..(j-5)..
-				",1;1,1;xdecor:"..def[j][1].."_cloud;"..def[j][1]..";]"
-	end
-	row2 = concat(row2)
-	for k = 9, 12 do
-		row3[#row3+1] = "item_image_button["..(k-9)..
-				",2;1,1;xdecor:"..def[k][1].."_cloud;"..def[k][1]..";]"
-	end
-	row3 = concat(row3)
-
-	local fs = {"size[8,7;]"..xbg..
-		row1..row2..row3..
-		"label[4,1.23;Cut]".."box[3.95,1;1.05,0.9;#555555]"..
-		"label[4,2.23;Repair]".."box[3.95,2;1.05,0.9;#555555]"..
-		"image[6,1;1,1;xdecor_saw.png]".."image[6,2;1,1;xdecor_anvil.png]"..
-		"image[7,2;1,1;hammer_layout.png]"..
-		"list[current_name;input;5,1;1,1;]".."list[current_name;output;7,1;1,1;]"..
-		"list[current_name;tool;5,2;1,1;]".."list[current_name;hammer;7,2;1,1;]"..
-		"button[4,0;2,1;craft;Crafting]"..
-		"button[6,0;2,1;storage;Storage]"..
-		"list[current_player;main;0,3.25;8,4;]"}
-	local formspec = concat(fs)
-
-	meta:set_string("formspec", formspec)
-	meta:set_string("infotext", "Work Table")
-
-	local inv = meta:get_inventory()
-	inv:set_size("output", 1)
+	inv:set_size("forms", 4*3)
 	inv:set_size("input", 1)
 	inv:set_size("tool", 1)
 	inv:set_size("hammer", 1)
+
+	local formspec = "size[8,7;]"..xbg..
+		"list[context;forms;4,0;4,3;]" ..
+		"label[0.95,1.23;Cut]box[-0.05,1;2.05,0.9;#555555]"..
+		"image[3,1;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
+		"label[0.95,2.23;Repair]box[-0.05,2;2.05,0.9;#555555]"..
+		"image[0,1;1,1;xdecor_saw.png]image[0,2;1,1;xdecor_anvil.png]"..
+		"image[3,2;1,1;hammer_layout.png]"..
+		"list[current_name;input;2,1;1,1;]"..
+		"list[current_name;tool;2,2;1,1;]list[current_name;hammer;3,2;1,1;]"..
+		"button[0,0;2,1;craft;Crafting]"..
+		"button[2,0;2,1;storage;Storage]"..
+		"list[current_player;main;0,3.25;8,4;]"
+
+	meta:set_string("formspec", formspec)
+	meta:set_string("infotext", "Work Table")
 end
 
 function worktable.fields(pos, _, fields, sender)
 	local player = sender:get_player_name()
 	local meta = minetest.get_meta(pos)
 	local inv = meta:get_inventory()
-	local inputstack = inv:get_stack("input", 1)
-	local outputstack = inv:get_stack("output", 1)
-	local outputcount = outputstack:get_count()
-	local inputname = inputstack:get_name()
-	local outputname = outputstack:get_name()
-	local shape, get, outputshape = {}, {}, {}
-	local name = dump(fields):match("%w+")
 
-	if outputcount < 99 and fields[name] then
-		outputshape = outputname:match(name)
-		if name ~= outputshape and outputcount > 0 then return end
-		shape = "xdecor:"..name.."_"..inputname:sub(9)
-		get = shape.." "..worktable.anz(name)
-
-		if minetest.registered_nodes[shape] then
-			inv:add_item("output", get)
-			inputstack:take_item()
-			inv:set_stack("input", 1, inputstack)
-		end
-	end
 	if fields.storage then
 		minetest.show_formspec(player, "", worktable.storage(pos))
 	end
@@ -141,20 +96,20 @@ function worktable.dig(pos, _)
 	local meta = minetest.get_meta(pos)
 	local inv = meta:get_inventory()
 
-	if not inv:is_empty("input") or not inv:is_empty("output") or not
+	if not inv:is_empty("input") or not inv:is_empty("forms") or not
 			inv:is_empty("hammer") or not inv:is_empty("tool") or not
-			inv:is_empty("storage") then
+			inv:is_empty("storage") or not inv:is_empty("craft") then
 		return false
 	end
 	return true
 end
 
-function worktable.put(_, listname, _, stack, _)
+function worktable.put(pos, listname, _, stack, _)
 	local stackname = stack:get_name()
 	local count = stack:get_count()
-	local mat = concat(material)
+	local mat = table.concat(material)
 
-	if listname == "output" then return 0 end
+	if listname == "forms" then return 0 end
 	if listname == "input" then
 		if stackname:find("default:") and mat:match(stackname:sub(9)) then
 			return count
@@ -172,9 +127,51 @@ function worktable.put(_, listname, _, stack, _)
 	return count
 end
 
+function worktable.take(pos, listname, index, stack, player)
+	if listname == "forms" then return -1 end
+	return stack:get_count()
+end
+
 function worktable.move(_, from_list, _, to_list, _, count, _)
 	if from_list == "storage" and to_list == "storage" then
 		return count else return 0 end
+end
+
+local function update_form_inventory(inv, input_stack)
+	if inv:is_empty("input") then
+		inv:set_list("forms", {})
+		return end
+
+	local form_inv_list = {}
+	for _, form in pairs(def) do
+		local material_name = input_stack:get_name():match("%a+:(%a+)")
+		local form_name = form[1]
+
+		local count = math.min(worktable.anz(form_name) * inv:get_stack("input", 1):get_count(), input_stack:get_stack_max())
+		table.insert(form_inv_list, string.format("xdecor:%s_%s %d", form_name, material_name, count))
+	end
+	inv:set_list("forms", form_inv_list)
+end
+
+function worktable.on_put(pos, listname, _, stack, _)
+	if listname == "input" then
+		local inv = minetest.get_meta(pos):get_inventory()
+		update_form_inventory(inv, stack)
+	end
+end
+
+function worktable.on_take(pos, listname, index, stack, player)
+	local inv = minetest.get_meta(pos):get_inventory()
+	if listname == "input" then
+		update_form_inventory(inv, stack)
+	elseif listname == "forms" then
+		local form_name = stack:get_name():match("%a+:(%a+)_%a+")
+		local input_stack = inv:get_stack("input", 1)
+		input_stack:take_item(math.ceil(stack:get_count() / worktable.anz(form_name)))
+		inv:set_stack("input", 1, input_stack)
+
+		update_form_inventory(inv, input_stack)
+	end
 end
 
 xdecor.register("worktable", {
@@ -189,12 +186,14 @@ xdecor.register("worktable", {
 	can_dig = worktable.dig,
 	on_construct = worktable.construct,
 	on_receive_fields = worktable.fields,
+	on_metadata_inventory_put = worktable.on_put,
+	on_metadata_inventory_take = worktable.on_take,
 	allow_metadata_inventory_put = worktable.put,
+	allow_metadata_inventory_take = worktable.take,
 	allow_metadata_inventory_move = worktable.move
 })
 
 local function description(m, w)
-	if m == "cloud" then return "" end
 	return m:gsub("%l", string.upper, 1).." "..w:gsub("%l", string.upper, 1)
 end
 
