0c4a296d2b88a96d75ebdaf794f068d60630512f
CloudyProton <34248863+CloudyProton@users.noreply.github.com>
2018-01-19 13:29:29 -0500

    Request: Change painting recipe to use dye group
    Request: Change painting craft recipe to use dye group instead of specifically blue dye.

--- src/recipes.lua
+++ src/recipes.lua
@@ -251,7 +251,7 @@ minetest.register_craft({
 minetest.register_craft({
 	output = "xdecor:painting_1",
 	recipe = {
-		{"default:sign_wall_wood", "dye:blue"}
+		{"default:sign_wall_wood", "group:dye"}
 	}
 })
 
